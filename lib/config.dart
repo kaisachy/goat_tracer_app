e573e6da74daac9b3d4a922953eb02cead9439e6
Gem Kaiser Santiago <santiagogemkaiser@gmail.com>
2025-11-09 13:45:14 +0800

    add https://ruminanttrace.ph

--- lib/config.dart
+++ lib/config.dart
@@ -3,10 +3,10 @@
 // }
 
 
-class AppConfig {
-  static const String baseUrl = 'http://192.168.100.145/cattle-tracer/public';
-}
-
 // class AppConfig {
-//   static const String baseUrl = 'https://ruminanttrace.ph';
+//   static const String baseUrl = 'http://192.168.100.145/cattle-tracer/public';
 // }
+
+class AppConfig {
+  static const String baseUrl = 'https://ruminanttrace.ph';
+}
